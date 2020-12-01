eaa8b496fa2479fd576c59a57f70515c66a321f9
Zubayr-dev <68279853+Zubayr-dev@users.noreply.github.com>
2020-12-01 08:53:54 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    document.querySelector('.sport').innerHTML=" The fight was not officially scored, but if it had been, Tyson would have handily beaten Jones Jr on account of landing more punches and looking faster and more aggressive across the eight two-minute rounds at an empty Staples Centre in Los Angeles.Despite being famed for his speed during his career, and fighting professionally as recently as 2018, the 51-year-old Jones Jr looked slower and was breathing raggedly far earlier than Tyson, 54. The fight had been billed as an exhibition, and in truth was barely more than an intense sparring session between two aged veterans — who looked every day of their 50-plus years from the moment they entered the ring.";
+    document.querySelector('.read-more').style.backgroundColor = "transparent";
+    document.querySelector('.read-more').style.color = "#2a2a2a";
+
+}
